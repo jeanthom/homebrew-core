b40b118a46a6a8f53a41055096680761757c8532
Jean THOMAS <jeanthom@users.noreply.github.com>
2021-07-12 16:24:10 +0200

    Fixes issues reported in #81031
     * openFPGALoader sources are fetched as a tarball rather than cloning the Git repository
     * Revision field was removed
     * An additional test was added ("--detect" with no device connected)

--- Formula/openfpgaloader.rb
+++ Formula/openfpgaloader.rb
@@ -1,11 +1,9 @@
 class Openfpgaloader < Formula
   desc "Universal utility for programming FPGA"
   homepage "https://github.com/trabucayre/openFPGALoader"
-  url "https://github.com/trabucayre/openFPGALoader.git",
-    tag:      "v0.4.0",
-    revision: "1dbbd34420e00070bd0f58e0a91a653e12a87b97"
+  url "https://github.com/trabucayre/openFPGALoader/archive/v0.4.0.tar.gz"
+  sha256 "ab29cc5cc758a5edf26944e7578a47a7887a70b87494ad516ef4b8241e6b481f"
   license "Apache-2.0"
-  revision 1
   head "https://github.com/trabucayre/openFPGALoader.git"
 
   depends_on "cmake" => :build
@@ -22,5 +20,8 @@ class Openfpgaloader < Formula
   test do
     version_output = shell_output("#{bin}/openFPGALoader -V 2>&1")
     assert_match "openFPGALoader v#{version}", version_output
+
+    error_output = shell_output("#{bin}/openFPGALoader --detect 2>&1 >/dev/null", 1)
+    assert_includes error_output, "JTAG init failed"
   end
 end
