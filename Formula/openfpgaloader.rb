36ce74c3d1851eef4feee4c15e16925d3a000a18
Jean THOMAS <25333063+jeanthom@users.noreply.github.com>
2021-12-02 07:24:43 +0100

    Fix trailing whitespace

--- Formula/openfpgaloader.rb
+++ Formula/openfpgaloader.rb
@@ -21,7 +21,7 @@ class Openfpgaloader < Formula
   depends_on "libftdi"
   depends_on "libusb"
   depends_on "zlib"
-  
+
   patch do
     url "https://github.com/trabucayre/openFPGALoader/commit/efeb0d83c479200e359407245f82000ee4f33558.patch?full_index=1"
     sha256 "7f15ac39f8d079ebe8e73a763bbb4e3d7b441f74df1d5586dbe15af967d5fc33"
